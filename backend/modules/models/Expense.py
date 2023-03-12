92092f3fd09a8e33cd55926dab71b83d7c2a067c
Ishan Sharma <is317576@dal.ca>
2023-03-12 15:24:39 -0300

    Added JWT Authentication for protected routes & added Exception Handling

--- backend/modules/models/Expense.py
+++ backend/modules/models/Expense.py
@@ -2,7 +2,7 @@ from database.database import db
 import uuid
 
 class Expense(db.EmbeddedDocument):
-    expense_id = db.UUIDField(binary=False,default=uuid.uuid1())
+    expense_id = db.UUIDField(binary=False,default=uuid.uuid4())
     group_id = db.StringField()
     spent_by = db.StringField()
     amount = db.FloatField()
--- backend/modules/models/Group.py
+++ backend/modules/models/Group.py
@@ -3,7 +3,7 @@ from modules.models.Expense import Expense
 import uuid
 
 class Group(db.Document):
-    group_id = db.UUIDField(binary=False,default=uuid.uuid1())
+    group_id = db.UUIDField(binary=False,default=uuid.uuid4())
     group_name = db.StringField(max_length = 100, required=True)
     group_description = db.StringField()
     expenses = db.EmbeddedDocumentListField(Expense)
