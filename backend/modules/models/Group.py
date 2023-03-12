21ee72422cf6b81a1e7422f61a5a76bf29959e1b
Ishan Sharma <is317576@dal.ca>
2023-03-12 15:39:23 -0300

    EMAT-33: Fields added in Group and Expense Models  as per discussion

--- backend/modules/models/Group.py
+++ backend/modules/models/Group.py
@@ -1,6 +1,7 @@
 from database.database import db
 from modules.models.Expense import Expense
 import uuid
+import datetime
 
 class Group(db.Document):
     group_id = db.UUIDField(binary=False,default=uuid.uuid4())
@@ -9,3 +10,7 @@ class Group(db.Document):
     expenses = db.EmbeddedDocumentListField(Expense)
     participants = db.ListField()
     group_currency = db.StringField()
+    isTemporary = db.BooleanField()
+    destruction_date = db.DateTimeField()
+    created_at = db.DateTimeField(default=datetime.datetime.utcnow())
+    created_by = db.StringField()
