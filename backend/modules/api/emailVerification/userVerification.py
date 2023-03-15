07650976672838da157c99bf062013a6e46e21c8
Nitesh <niteshkumar@dal.ca>
2023-03-15 03:29:25 -0300

    API for verification of OTP during User registration

--- backend/modules/api/emailVerification/userVerification.py
+++ backend/modules/api/emailVerification/userVerification.py
@@ -11,11 +11,11 @@ import random
 app = Flask(__name__)
 app.config.update(
     DEBUG=True,
-    MAIL_SERVER = "smtp.gmail.com",
+    MAIL_SERVER = "smtp.mail.yahoo.com.",
     MAIL_PORT = 587,
     MAIL_USE_TLS = True,
-    MAIL_USERNAME = "NAME@gmail.com",
-    MAIL_PASSWORD = "Password"  
+    MAIL_USERNAME = "ematemailverification@yahoo.com",
+    MAIL_PASSWORD = "EjK6zTfZgwwySxK"  
 
 )
 
@@ -30,12 +30,14 @@ def RegistrationVerificationCode():
 
 #Sending verification email for user registration
 
+# user's email as parameter into argument = to
+
 def sendVerificationEmail(to):
 
     verificationCode = RegistrationVerificationCode()
     message = Message(
 
-        subject = "User registration code email",
+        subject = "User registration code",
         recipients = [to],
         body = f"Your verification code is {verificationCode}."
     )
@@ -43,5 +45,78 @@ def sendVerificationEmail(to):
     mail.send(message)
 
     return verificationCode
+
+
+# Using function in route to send and verify the code
+
+#dictionary to store verification code, email as key and code as pair.
+verificationCodes = {}
+
+@app.route("/userRegistrationMail", methods = ["POST"])
+
+def userRegistrationMail():
+
+    email = request.form.get("email")
+
+    #If no email is provided
+    if not email:
+        return "Email address is required.", 400
+    
+    subject = request.form.get("subject", "Verification code")
+    body = request.form.get("body", "")
+
+    #error handling using try-catch block
+    #if any exception arise, trigger 500 response
+
+    try:
+        code = userRegistrationMail(email)
+        verificationCodes[email] = code
+    
+    except Exception as e:
+        return f"Unable to send email: {str(e)}", 500
+    return "Verification code sent succefully."
+
+
+#API to verify the verification code
+
+@app.route("/verifyCode", methods = ["POST"])
+
+def verifyCode():
+
+    email = request.form.get("email")
+    code = request.form.get("code")
+
+    #if email doesn't match with the vefification code
+
+    if email or not code:
+        return "Email and verification code are required.", 400
+    
+    if email not in verificationCodes:
+        return "No verification code found for this email.", 404
+    
+    if verificationCodes[email] != code:
+        return "Invalid verification code. Please request a new code", 401
+    
+    #Deleting verification code, so that new code can be store.
+    del verificationCodes[email]
+
+
+    #Email response format and error handling
+
+    try:
+        message = Message(
+            subject = "Email confirmed",
+            recipients = [email],
+            body = "User's email has been verified."
+        )
+        mail.send(message)
+
+    except Exception as e:
+        return f"Failed to send email: {str(e)}", 500
+    
+
+    return "Email sent successfully."
+    
+
 
 
