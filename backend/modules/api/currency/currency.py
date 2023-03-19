4cb37c0c612371a528a3ee9f0fd610bbdae25d70
Nitesh <niteshkumar@dal.ca>
2023-03-19 18:19:27 -0300

    Add currency details API and get name, email APIs

--- backend/modules/api/currency/currency.py
+++ backend/modules/api/currency/currency.py
@@ -9,9 +9,24 @@ def getCurrencyList():
 
     try:
 
-        return jsonify({"currency list":CurrencyList.objects.all().values_list('name')})
+        return jsonify({"status": True, "currency list":CurrencyList.objects.all().values_list('name')}), 200
 
     except (FieldDoesNotExist, InvalidQueryError):
-        return jsonify({"error": "Could not get the currency list"}), 400
+        return jsonify({"status": False, "error": "Could not get the currency list"}), 400
     except Exception as e:
-        return jsonify({"error": str(e)}), 500
+        return jsonify({"status": False, "error": str(e)}), 500
+    
+
+@currency.route("/details", methods=["GET"])
+def getCurrency():
+
+    try:
+
+        details = CurrencyList.objects().values("name", "symbol", "code")
+        
+        return jsonify({"status": True, "message": list(details)}), 200
+
+    except (FieldDoesNotExist, InvalidQueryError):
+        return jsonify({"status": False, "error": "Could not get the currency details"}), 400
+    except Exception as e:
+        return jsonify({"status": False, "error": str(e)}), 500
