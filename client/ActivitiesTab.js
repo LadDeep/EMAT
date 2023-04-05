1abf75f4f6200c91cf24dd8618f988dcd642ac96
Ishan Sharma <ishansharma1320@gmail.com>
2023-04-05 02:51:20 -0300

    EMAT-35: Activities Page Integration with APIs

--- client/ActivitiesTab.js
+++ client/ActivitiesTab.js
@@ -1,37 +1,98 @@
-import React from 'react';
+import {React,useEffect,useState} from 'react';
 import { StyleSheet, ScrollView, Text, View,Image } from 'react-native';
+import {FetchActivitiesList, FetchOtherUserProfile} from './src/api/api';
+import { getValueFor } from './src/secureStore';
+// const activities = [
+//   { id: 1, title: 'Dinner with friends', date: '3/10/23', amount: -40 },
+//   { id: 2, title: 'Uber ride', date: '3/11/23', amount: 15 },
+//   { id: 3, title: 'Movie tickets', date: '3/12/23', amount: -25 },
+//   { id: 4, title: 'Grocery shopping', date: '3/14/23', amount: 80 },
+//   { id: 2, title: 'Uber ride', date: '3/11/23', amount: 15 },
+//   { id: 3, title: 'Movie tickets', date: '3/12/23', amount: -25 },
+//   { id: 4, title: 'Grocery shopping', date: '3/14/23', amount: 80 },
+//   { id: 3, title: 'Movie tickets', date: '3/12/23', amount: -25 },
+//   { id: 4, title: 'Grocery shopping', date: '3/14/23', amount: 80 },
+//   { id: 3, title: 'Movie tickets', date: '3/12/23', amount: -25 },
+//   { id: 4, title: 'Grocery shopping', date: '3/14/23', amount: 80 },
+//   { id: 3, title: 'Movie tickets', date: '3/12/23', amount: -25 },
+//   { id: 4, title: 'Grocery shopping', date: '3/14/23', amount: 80 },
+// ];
 
-const activities = [
-  { id: 1, title: 'Dinner with friends', date: '3/10/23', amount: -40 },
-  { id: 2, title: 'Uber ride', date: '3/11/23', amount: 15 },
-  { id: 3, title: 'Movie tickets', date: '3/12/23', amount: -25 },
-  { id: 4, title: 'Grocery shopping', date: '3/14/23', amount: 80 },
-  { id: 2, title: 'Uber ride', date: '3/11/23', amount: 15 },
-  { id: 3, title: 'Movie tickets', date: '3/12/23', amount: -25 },
-  { id: 4, title: 'Grocery shopping', date: '3/14/23', amount: 80 },
-  { id: 3, title: 'Movie tickets', date: '3/12/23', amount: -25 },
-  { id: 4, title: 'Grocery shopping', date: '3/14/23', amount: 80 },
-  { id: 3, title: 'Movie tickets', date: '3/12/23', amount: -25 },
-  { id: 4, title: 'Grocery shopping', date: '3/14/23', amount: 80 },
-  { id: 3, title: 'Movie tickets', date: '3/12/23', amount: -25 },
-  { id: 4, title: 'Grocery shopping', date: '3/14/23', amount: 80 },
-];
+
 
 const Activities = () => {
+  const [activities, setActivities] = useState([]);
+  const [ownUserID,setOwnUserID] = useState(undefined);
+  
+  async function fetchUserIdFromSecureStore(){
+    let ownUser = await getValueFor("USER_ID");
+    setOwnUserID(ownUser);
+  }
+  useEffect(() => {
+    
+
+    fetchUserIdFromSecureStore();
+    FetchActivitiesList(
+      (res) => {
+        if(res.data.status){
+            let response = res.data.response;
+            
+            if(Array.isArray(response)){
+              let user_ids =  [... new Set(response.map(item => item.spent_by))];
+              
+            FetchOtherUserProfile({user_id: user_ids},(userResponse)=>{
+              if(userResponse.data.status){
+                let userResponseObject = userResponse.data.response;
+                
+                if(Array.isArray(userResponseObject)){
+                  let userResponseIDNameMapping = {};
+                  userResponseObject.forEach(item=>{
+                    userResponseIDNameMapping[item.user_id] = `${item.first_name} ${item.last_name}`;
+                  })
+                  response = response.map(item=>{
+                    item['created_at'] = new Date(item['created_at']['$date']).toLocaleDateString('en-GB');
+                    item['user_name'] = userResponseIDNameMapping[item.spent_by];
+                    return item;
+                  })
+                  setActivities(response);
+                }
+              }
+            },(userError)=>{
+              console.log(userError);
+            })
+            }
+            
+        }
+        // setCurrencyList(res.data.message)
+      },
+      (err) => {
+        console.log(err);
+      }
+    );
+  },[]);
+
   const renderActivities = () => {
     return activities.map((activity) => {
-      const amountStyle = activity.amount < 0 ? styles.amountNegative : styles.amountPositive;
-      const formattedAmount = `${activity.amount > 0 ? '+' : '-'}$${Math.abs(activity.amount)}`;
-      const formattedLabel = `${activity.amount > 0 ? 'You Get Back' : 'You Owe'}`;
+      let amountStyle = styles.amountNegative;
+      let formattedAmount = `- $${Math.abs(activity.amount).toFixed(2)}`;
+      let formattedLabel = `You Owe`;
+      
+      if(ownUserID === activity.spent_by){
+        amountStyle = styles.amountPositive;
+        formattedAmount = `+ $${Math.abs(activity.amount).toFixed(2)}`;
+        formattedLabel = 'You Get Back'
+      }
+      
+
       return (
       //   <View style={styles.Maincontainer}>
       //    <Image 
       //   source={require('./assets/download.jpeg')} 
       //   style={styles.profileImage} 
       // />
-        <View key={activity.id} style={styles.activity}>
-          <Text style={styles.title}>#name_of_user added "{activity.title}".</Text>
-          <Text style={styles.date}>{activity.date}</Text>
+        <View key={activity.expense_id} style={styles.activity}>
+          <Text style={styles.title}>{activity.user_name} "{activity.description}".</Text>
+          <Text style={styles.date}>{activity.created_at}</Text>
           <Text style={amountStyle}>{formattedLabel}  {formattedAmount}</Text>
         </View>
         // </View>
