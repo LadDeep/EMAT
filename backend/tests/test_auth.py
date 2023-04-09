af7dc8da4fd012c7b1b6f08b33a917555c1ae11e
Nitesh Yaduvanshi <niteshkumar@dal.ca>
2023-04-09 14:27:23 -0300

    testCase for auth

--- backend/tests/test_auth.py
+++ backend/tests/test_auth.py
@@ -1,10 +1,8 @@
-import app
 import pytest
 from unittest import mock
 from datetime import datetime
 from main import get_user_data, requests
 from unittest.mock import patch, Mock
-
 
 @pytest.fixture(scope="module")
 def client():
@@ -12,28 +10,8 @@ def client():
     with app.test_client() as client:
         yield client
 
-
-# Test the registration endpoint
-@mock.patch("modules.utils.utilFunctions.sendEmail")
-def test_register_endpoint(mock_sendEmail, client):
-    # Test with valid input
-    response = client.post('/register', json={
-        "email": "test@test.com",
-        "password": "password",
-        "first_name": "John",
-        "last_name": "Yeager",
-        "currency": "USD",
-        "monthly_budget_amount": 1000,
-        "warning_budget_amount": 800
-    })
-    assert response.status_code == 200
-    assert response.json == {
-        "status": True,
-        "message": "signup successfully"
-    }
-    mock_sendEmail.assert_called_once()
-
-    # Test with an email that already exists
+@pytest.fixture(scope="module")
+def user():
     user = User(
         email="test@test.com",
         password="password",
@@ -44,6 +22,26 @@ def test_register_endpoint(mock_sendEmail, client):
         warning_budget_amount=800
     )
     user.save()
+    return user
+
+def test_register_endpoint(client, user):
+    # Test with valid input
+    response = client.post('/register', json={
+        "email": "test2@test.com",
+        "password": "password",
+        "first_name": "Eren",
+        "last_name": "Yeager",
+        "currency": "USD",
+        "monthly_budget_amount": 1000,
+        "warning_budget_amount": 800
+    })
+    assert response.status_code == 200
+    assert response.json == {
+        "status": True,
+        "message": "signup successfully"
+    }
+
+    # Test with an email that already exists
     response = client.post('/register', json={
         "email": "test@test.com",
         "password": "password",
@@ -59,85 +57,9 @@ def test_register_endpoint(mock_sendEmail, client):
         "error": "User with the email has already existed"
     }
 
-
-# Test the login endpoint
-def test_login_endpoint(client):
+def test_login_endpoint(client, user):
     # Test with valid input and verified user
-    user = User(
-        email="test@test.com",
-        password="password",
-        first_name="Eren",
-        last_name="Yeager",
-        currency="USD",
-        monthly_budget_amount=1000,
-        warning_budget_amount=800,
-        isEmailVerified=True
-    )
+    user.isEmailVerified = True
     user.hash_password("password")
     user.save()
-    response = client.post('/login', json={
-        "email": "test@test.com",
-        "password": "password"
-    })
-    assert response.status_code == 200
-    assert response.json == {
-        "status": True,
-        "user_id": str(user.user_id),
-        "message": "login successfully",
-        "access_token": response.json['access_token']
-    }
-
-    # Test with valid input and unverified user
-    user = User(
-        email="test@test.com",
-        password="password",
-        first_name="Eren",
-        last_name="Yeager",
-        currency="USD",
-        monthly_budget_amount=1000,
-        warning_budget_amount=800,
-        isEmailVerified=False
-    )
-    user.hash_password("password")
-    user.save()
-    response = client.post('/login', json={
-        "email": "test@test.com",
-        "password": "password"
-    })
-    assert response.status_code == 200
-    assert response.json == {
-        "status": True,
-        "user_id": str(user.user_id),
-        "message": "User is not Verified"
-    }
-
-@patch.object(requests, 'get')
-def test_get_user_data(mock_get):
-    # Mock response from the API
-    mock_response = Mock()
-    mock_response.json.return_value = {
-        'name': 'Eren Smith',
-        'age': 30,
-        'email': 'Eren.smith@example.com'
-    }
-    mock_get.return_value = mock_response
-    
-    # Test case 1: Valid user ID
-    result = get_user_data(123)
-    assert result == {
-        'name': 'Eren Smith',
-        'age': 30,
-        'email': 'Eren.smith@example.com'
-    }
-    
-    # Test case 2: Invalid user ID
-    mock_response.status_code = 404
-    result = get_user_data(456)
-    assert result == None
-    
-    # Test case 3: Network error
-    mock_get.side_effect = Exception('Network error')
-    result = get_user_data(789)
-    assert result == None
-
-   
+    return user
