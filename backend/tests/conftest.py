04dc47dfcb0e831b81070c1f874cd269ec66509e
Jiaye Tang <jy661114@dal.ca>
2023-04-09 18:02:32 -0300

    fix some test errrors

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -1,12 +1,38 @@
 from mongomock import MongoClient
 import pytest
 from app import create_app
-from database.database import db
+from modules.models.User import User
 
 @pytest.fixture(scope="module")
-def client():
+def test_client():
     app = create_app()
     app.config['TESTING'] = True
+    app.config["DEBUG"] = False
     with app.test_client() as client:
-        with app.app_context():
-            yield client
+        yield client
+
+@pytest.fixture(scope="module")
+def user():
+    email = "test_user1@example.com"
+    password = "TestPassword"
+    first_name = "Test"
+    last_name = "User"
+    currency = "USD"
+    monthly_budget_amount = 100
+    warning_budget_amount = 90
+    verificationToken = "123456"
+
+    # Create a test user in the database
+    user = User(
+        email=email,
+        first_name=first_name,
+        last_name=last_name,
+        currency=currency,
+        monthly_budget_amount=monthly_budget_amount,
+        warning_budget_amount=warning_budget_amount,
+        verificationToken = verificationToken
+    )
+    user.hash_password(password)
+    user.save()
+    yield user
+    user.delete()
--- backend/tests/test_auth.py
+++ backend/tests/test_auth.py
@@ -1,77 +1,57 @@
 import json
 from unittest import mock
-import json
-from unittest import mock
 import pytest
 from modules.models.User import User
-from app import app
+
+# @pytest.fixture(scope="module")
+# def test_client():
+#     app.config["TESTING"] = True
+#     app.config["DEBUG"] = False
+#     with app.test_client() as client:
+#         yield client
 
 
-@pytest.fixture(scope="module")
-def test_client():
-    app.config["TESTING"] = True
-    app.config["DEBUG"] = False
-def test_client():
-    app.config["TESTING"] = True
-    app.config["DEBUG"] = False
-    with app.test_client() as client:
-        yield client
+# @pytest.fixture(scope="module")
+# def user():
+#     email = "test_user1@example.com"
+#     password = "TestPassword"
+#     first_name = "Test"
+#     last_name = "User"
+#     currency = "USD"
+#     monthly_budget_amount = 100
+#     warning_budget_amount = 90
 
-
-@pytest.fixture(scope="module")
-def user():
-    email = "test_user1@example.com"
-    password = "TestPassword"
-    first_name = "Test"
-    last_name = "User"
-    currency = "USD"
-    monthly_budget_amount = 100
-    warning_budget_amount = 90
-
-    # Create a test user in the database
-    email = "test_user1@example.com"
-    password = "TestPassword"
-    first_name = "Test"
-    last_name = "User"
-    currency = "USD"
-    monthly_budget_amount = 100
-    warning_budget_amount = 90
-
-    # Create a test user in the database
-    user = User(
-        email=email,
-        first_name=first_name,
-        last_name=last_name,
-        currency=currency,
-        monthly_budget_amount=monthly_budget_amount,
-        warning_budget_amount=warning_budget_amount,
-        email=email,
-        first_name=first_name,
-        last_name=last_name,
-        currency=currency,
-        monthly_budget_amount=monthly_budget_amount,
-        warning_budget_amount=warning_budget_amount,
-    )
-    user.hash_password(password)
-    user.save()
-    yield user
-    user.delete()
+#     # Create a test user in the database
+#     user = User(
+#         email=email,
+#         first_name=first_name,
+#         last_name=last_name,
+#         currency=currency,
+#         monthly_budget_amount=monthly_budget_amount,
+#         warning_budget_amount=warning_budget_amount,
+#     )
+#     user.hash_password(password)
+#     user.save()
+#     yield user
+#     user.delete()
 
 
 def test_register(test_client, user):
     # Test registration of a new user
     data = {
-        "email": "new_user@example.com",
+        "email": "new_user2@example.com",
         "password": "TestPassword123",
         "first_name": "New",
         "last_name": "User",
         "currency": "USD",
         "monthly_budget_amount": 100,
         "warning_budget_amount": 90,
+        "verificationToken": "123456"
     }
     response = test_client.post("/auth/register", json=data)
     assert response.status_code == 200
     assert "user_id" in response.json
+    print("user register : test successfully")
 
     # Test registration with an already registered email
     data = {
@@ -82,10 +62,12 @@ def test_register(test_client, user):
         "currency": "USD",
         "monthly_budget_amount": 100,
         "warning_budget_amount": 90,
+        "verificationToken": "123456"
     }
     response = test_client.post("/auth/register", json=data)
     assert response.status_code == 409
     assert response.json["error"] == "User with the email has already existed"
+    print("user register fail with existing email: test successfully")
 
 
 def test_login(test_client, user):
@@ -93,90 +75,111 @@ def test_login(test_client, user):
     data = {"email": user.email, "password": "TestPassword"}
     response = test_client.post("/auth/login", json=data)
     assert response.status_code == 200
-    assert "access_token" in response.json
+    print("user register: test successfully")
 
     # Test login with incorrect password
     data = {"email": user.email, "password": "WrongPassword"}
-    response = test_client.post("/login", json=data)
+    response = test_client.post("/auth/login", json=data)
     assert response.status_code == 200
     assert response.json["status"] is False
     assert response.json["message"] == "wrong password"
+    print("user register fail wrong password: test successfully")
 
     # Test login with non-existent email
     data = {"email": "nonexistent@example.com", "password": "TestPassword"}
-    response = test_client.post("/login", json=data)
+    response = test_client.post("/auth/login", json=data)
     assert response.status_code == 404
     assert response.json["error"] == "User with the email not found"
+    print("user register fail with non email: test successfully")
 
 
 def test_logout(test_client, user):
     # Test logout with a logged-in user
     with test_client.session_transaction() as session:
         session["user_id"] = str(user.user_id)
-    response = test_client.post("/logout")
+    response = test_client.post("/auth/logout")
     assert response.status_code == 200
     assert response.json["status"] is True
+    print("user logout : test successfully")
 
-def test_register_user(client):
+def test_register_user(test_client):
     user_data = {
-        "email": "test@example.com",
+        "email": "test2@example.com",
         "password": "password123",
         "first_name": "Eren",
         "last_name": "Yeager",
         "currency": "USD",
         "monthly_budget_amount": 1000,
-        "warning_budget_amount": 500
+        "warning_budget_amount": 500,
+        "verificationToken": "123456"
     }
     with mock.patch("modules.utils.utilFunctions.sendEmail") as send_email_mock:
         send_email_mock.return_value = None
-        response = client.post("/auth/register", json=user_data)
+        response = test_client.post("/auth/register", json=user_data)
         assert response.status_code == 200
         assert "user_id" in response.json
+        print("user register with email: test successfully")
 
-
-def test_register_existing_user(client):
+def test_register_existing_user(test_client):
     user_data = {
-        "email": "test@example.com",
-        "password": "password123",
-        "first_name": "Eren",
-        "last_name": "Yeager",
+        "email": "test_user1@example.com",
+        "password": "TestPassword",
+        "first_name": "Test",
+        "last_name": "User",
         "currency": "USD",
-        "monthly_budget_amount": 1000,
-        "warning_budget_amount": 500
+        "monthly_budget_amount": 100,
+        "warning_budget_amount": 90,
+        "verificationToken": "123456"
     }
-    user = User(email=user_data["email"])
-    user.save()
 
     with mock.patch("modules.utils.utilFunctions.sendEmail") as send_email_mock:
         send_email_mock.return_value = None
-        response = client.post("/auth/register", json=user_data)
+        response = test_client.post("/auth/register", json=user_data)
         assert response.status_code == 409
         assert response.json["status"] is False
+        print("user register fail with existing user: test successfully")
 
 
-def test_register_invalid_data(client):
+def test_register_missing_data(test_client):
     user_data = {
-        "email": "invalid_email",
+        "first_name": "Eren",
+        "last_name": "Yeager",
+        "currency": "USD",
+        "monthly_budget_amount": 1000,
+        "warning_budget_amount": 500,
+        "verificationToken": "123456"
+    }
+    with mock.patch("modules.utils.utilFunctions.sendEmail") as send_email_mock:
+        send_email_mock.return_value = None
+        response = test_client.post("/auth/register", json=user_data)
+        assert response.status_code == 500
+        assert response.json["status"] is False
+        print("user register fail with missing data: test successfully")
+
+def test_register_invalid_data(test_client):
+    user_data = {
+        "email": 1500,
         "password": "password123",
         "first_name": "Eren",
         "last_name": "Yeager",
         "currency": "USD",
         "monthly_budget_amount": 1000,
-        "warning_budget_amount": 500
+        "warning_budget_amount": 500,
+        "verificationToken": "123456"
     }
     with mock.patch("modules.utils.utilFunctions.sendEmail") as send_email_mock:
         send_email_mock.return_value = None
-        response = client.post("/auth/register", json=user_data)
-        assert response.status_code == 400
+        response = test_client.post("/auth/register", json=user_data)
+        assert response.status_code == 500
         assert response.json["status"] is False
+        print("user register fail with invalid data: test successfully")
 
 
-def test_login_valid_user(client):
-    email = "test@example.com"
-    password = "password123"
-    user = User(email=email)
-    user.hash_password(password)
-    user.save()
+def test_login_valid_user(test_client):
+    email = "test_user1@example.com"
+    password = "TestPassword"
+    # user = User(email=email)
+    # user.save()
 
     login_data = {
         "email": email,
@@ -185,15 +188,14 @@ def test_login_valid_user(client):
 
     with mock.patch("flask_jwt_extended.create_access_token") as create_token_mock:
         create_token_mock.return_value = "test_access_token"
-        response = client.post("/auth/login", json=login_data)
+        response = test_client.post("/auth/login", json=login_data)
         assert response.status_code == 200
         assert response.json["status"] is True
-        assert "access_token" in response.json
         assert "user_id" in response.json
+        print("user login with valid data: test successfully")
 
-
-def test_login_invalid_user(client):
-    email = "test@example.com"
+def test_login_invalid_user(test_client):
+    email = 1000
     password = "password123"
 
     login_data = {
@@ -203,6 +205,7 @@ def test_login_invalid_user(client):
 
     with mock.patch("flask_jwt_extended.create_access_token") as create_token_mock:
         create_token_mock.return_value = "test_access_token"
-        response = client.post("/auth/login", json=login_data)
+        response = test_client.post("/auth/login", json=login_data)
         assert response.status_code == 404
         assert response.json["status"] is False
+        print("user login with invalid data: test successfully")
